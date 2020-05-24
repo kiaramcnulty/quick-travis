93ceaf0ac26f5df537fddaea9606f73fdce473ad
Kiara McNulty <kiaramcnulty@gmail.com>
2020-05-24 12:05:35 -0700

    Delete app.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders current directory text', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Current Directory/i);
-  expect(linkElement).toBeInTheDocument();
-});
